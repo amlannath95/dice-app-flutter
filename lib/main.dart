e6e46453533ac353e55ceb5ad9b83191eaf95b6f
amlannath95 <amlannath95@gmail.com>
2020-04-02 20:11:06 +0530

    Images added. Expanded widget added.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text('Dicee'),
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +20,17 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
